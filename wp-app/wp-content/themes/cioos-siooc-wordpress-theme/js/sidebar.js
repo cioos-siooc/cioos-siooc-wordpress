4136b621abe996545e0ccb889b02ab15664536f8
Naumann Julien <julien.naumann@gmail.com>
2021-06-11 10:59:08 -0400

    Add sidebar generator page in admin panel + add metaBox Sidebar in Post

---
+++ wp-app/wp-content/themes/cioos-siooc-wordpress-theme/js/sidebar.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+  if(jQuery('tr.meta_layout input[value=option2]').is(":checked")){
+    jQuery('tr.meta_sidebar').show();
+  }
+  else if(jQuery('tr.meta_layout input[value=option2]').not(":checked")){
+    jQuery('tr.meta_sidebar').hide();
+  }
+});
+
+jQuery(document).ready(function(){
+  jQuery('input[type=radio]').change(function() {
+    if(jQuery('tr.meta_layout input[value=option2]').is(":checked")){
+      jQuery('tr.meta_sidebar').fadeIn();
+    }
+    else if(jQuery('tr.meta_layout input[value=option2]').not(":checked")){
+      jQuery('tr.meta_sidebar').fadeOut();
+    }
+  });
+});
