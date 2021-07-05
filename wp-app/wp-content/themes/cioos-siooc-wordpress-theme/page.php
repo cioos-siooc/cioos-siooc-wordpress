4085bfc64518a38e3b10a0eba35acdefa91d8f8b
Naumann Julien <julien.naumann@gmail.com>
2021-07-05 11:44:56 -0400

    Link between sidebar edit page configuration and front end page

--- wp-app/wp-content/themes/cioos-siooc-wordpress-theme/page.php
+++ wp-app/wp-content/themes/cioos-siooc-wordpress-theme/page.php
@@ -18,18 +18,22 @@ get_header();
 	<main id="primary" class="site-main">
 		<div class="container">
 			<?php
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
+				if ( cioos_meta_layout()== "option2" and cioos_meta_sidebars()) : 
+					get_sidebar();
 				endif;
+			
+				while ( have_posts() ) :
+					the_post();
 
-			endwhile; // End of the loop.
-		?>
+					get_template_part( 'template-parts/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+
+				endwhile; // End of the loop.
+			?>
 		</div>
 	</main><!-- #main -->
 
