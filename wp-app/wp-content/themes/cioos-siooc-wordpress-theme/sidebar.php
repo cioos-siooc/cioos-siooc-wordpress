3416f057652e8ac98f41830eb7e6bff1c4126bec
Naumann Julien <julien.naumann@gmail.com>
2021-07-06 15:46:19 -0400

    add CSS for sidebar

--- wp-app/wp-content/themes/cioos-siooc-wordpress-theme/sidebar.php
+++ wp-app/wp-content/themes/cioos-siooc-wordpress-theme/sidebar.php
@@ -10,10 +10,15 @@
 
 	<div id="sidebar">
 	<div id="sidebar-core">
-
-		<?php 
-			dynamic_sidebar( cioos_meta_sidebars() )
-		?>
+		<?php if ( ! dynamic_sidebar( cioos_meta_sidebars() ) ) : ?>
+			<aside class="widget widget_text">
+				<h3 class="widget-title"><?php _e( 'Please Add Widgets', 'experon' ); ?></h3>
+				<div class="textwidget"><div class="error-icon">
+					<p><?php _e( 'Remove this message by adding widgets to the Sidebar from the Widgets section of the Wordpress admin area.', 'experon' ); ?></p>
+					<a href="<?php echo esc_url( admin_url( 'widgets.php' ) ); ?>" title="No Widgets Selected"><?php _e( 'Click here to go to Widget area.', 'experon' ); ?></a>
+				</div></div>
+			</aside>
+		<?php endif; ?>
 
 	</div>
 	</div><!-- #sidebar -->
