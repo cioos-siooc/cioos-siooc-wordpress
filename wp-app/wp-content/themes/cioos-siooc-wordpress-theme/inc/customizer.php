c71c8710a800f49a71ef48a1d3a9e869accac733
Scott Bruce <scott.bruce@mi.mun.ca>
2021-08-02 14:22:59 -0230

    Added theme option for pulling list of organizations from CKAN into Wordpress
    Added setting "ra_catalogue_api_url" to allow a base URL for a CKAN installation to be added to an RA's settings.  The URL will be used to query the CKAN API and pull down a list of organizations listed in the CKAN server, with paths to logos, site URLs, and proper names (according to selected language).
    
    The list of organizations is written out with links pointing back to the organization page listed on the CKAN install.
    
    Currently, formatted to work with Gutenberg, but could be templated or reformatted to something more generic.

--- wp-app/wp-content/themes/cioos-siooc-wordpress-theme/inc/customizer.php
+++ wp-app/wp-content/themes/cioos-siooc-wordpress-theme/inc/customizer.php
@@ -54,6 +54,17 @@ function cioos_customize_register( $wp_customize ) {
 		),
 	) );
 
+	$wp_customize->add_setting( 'ra_catalogue_api_url', array(
+		'default' => '',
+  		'transport' => 'refresh'
+	  ) );
+	$wp_customize->add_control( 'ra_catalogue_api_url', array(
+		'type' => 'text',
+		'section' => 'title_tagline', 
+		'label' => __( 'R.A. CKAN Base URL' ),
+		'description' => __( 'Pulls a list of data contributors from the referenced CKAN organization list that contain at least 1 dataset.  Example: https://catalogue.[hostname]/ or https://[hostname]/ckan/.  Use the [ckan_organizations] shortcode to place the list in a page/post.' ),
+	) );
+
 	$wp_customize->add_setting( 'search_enable', array(
 		'default' => false,
   		'transport' => 'refresh'
@@ -107,3 +118,83 @@ function cioos_customize_register( $wp_customize ) {
 
 }
 add_action( 'customize_register', 'cioos_customize_register' );
+
+function fetch_ckan_organizations(){
+
+	$return_value = "<p>NO CKAN API URL SPECIFIED!</p>";
+
+	$base_url = rtrim(get_theme_mod("ra_catalogue_api_url"), '/');
+
+	if(!empty($base_url)){
+		// Get language code for currently selected language
+		$lang = strtolower(substr(get_locale(), 0, 2));
+
+		$curl = curl_init();
+
+		$org_list = $base_url . "/api/3/action/organization_list";
+
+		curl_setopt($curl, CURLOPT_URL, $org_list);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 1);
+	
+		$output = curl_exec($curl);
+
+		$organizations = json_decode($output, true)["result"];
+		$org_details = [];
+		foreach($organizations as $key => $org_id){
+			$org_detail_url = $base_url . "/api/3/action/organization_show?id=" . $org_id;
+			curl_setopt($curl, CURLOPT_URL, $org_detail_url);
+			
+			$org_detail_output = curl_exec($curl);
+			$info = json_decode($org_detail_output, true)["result"];
+			
+			// Add organizations with at least 1 dataset
+			if ($info["package_count"] >= 1) {
+				$org_details[$org_id]["id"] = $org_id;
+				$org_details[$org_id]["name"] = $info["title_translated"];
+				$org_details[$org_id]["logo"] = $info["image_url_translated"];
+				$org_details[$org_id]["site_url"] = $info["external_home_url"];
+				$org_details[$org_id]["ckan_org_url"] = $base_url . "/organization/" . $org_id;
+			}
+		}
+		
+		$_org_detail_template  = "  <div class=\"wp-block-column\">";
+		$_org_detail_template .= "    <figure class=\"wp-block-image size-large\">";
+		$_org_detail_template .= "      <a href=\"{ckan_org_url}\">";
+		$_org_detail_template .= "      <img src=\"{logo}\" loading=\"lazy\" alt=\"{name}\" />";
+		$_org_detail_template .= "      </a>";
+		$_org_detail_template .= "    </figure>";
+		$_org_detail_template .= "  </div>";
+
+		$return_value = "<div class=\"wp-block-columns ra_ckan_org_list partners bottom-3\">";
+		foreach($org_details as $key => $org){
+			$_org_detail = $_org_detail_template;
+			$_org_detail = str_replace("{name}", $org["name"][$lang], $_org_detail);
+			$_org_detail = str_replace("{ckan_org_url}", $org["ckan_org_url"], $_org_detail);
+			$_org_detail = str_replace("{site_url}", $org["site_url"], $_org_detail);
+
+			$logo_path =  $org["logo"][$lang];
+
+			// Determine if a logo path is a full URL or if it is a relataive 
+			// URL, which implies that it a path relative to the root of the 
+			// CKAN base URL.
+			if(!filter_var($logo_path, FILTER_VALIDATE_URL)){
+				$source_url = parse_url($base_url);
+				$logo_path = sprintf("%s://%s/%s", $source_url["scheme"], $source_url["host"], $logo_path);
+			}
+
+			$_org_detail = str_replace("{logo}", $logo_path, $_org_detail);
+			
+			$return_value .= $_org_detail;
+		}
+		
+		$return_value .= "</div>";
+		curl_close($curl);
+	}
+
+	// return "<p><strong>[SHORT CODE PLACEHOLDER]: $base_url </strong></p>";
+	// return get_locale();
+	return $return_value;
+}
+
+add_shortcode('ckan_organizations', 'fetch_ckan_organizations');
